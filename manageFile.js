5ff6564307fa28636be0d29058496c60ebebbc55
Carolina Morales <carovms@gmail.com>
2021-06-14 09:50:48 -0400

    validate listo

--- manageFile.js
+++ manageFile.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 const markdownLinkExtractor = require('markdown-link-extractor');
 let path = require('path');
+const fetch =  require('node-fetch');
+const { resolve } = require('path');
 
 const mdlinksReading = (path, options) => {
-
     let files = fs.readdirSync(path);
     files = files.filter(file => file.includes('.md'));
 
     return linkExtractor(files, options);
+
 }
 
 const linkExtractor = (files, options) => {
@@ -16,25 +18,44 @@ const linkExtractor = (files, options) => {
         const markdown = fs.readFileSync(file, {encoding: 'utf8'});
 
         const details = markdownLinkExtractor(markdown, true);
-        let linksFiltered = details.filter(detail => detail.includes('http'));
-        
+
+        let linksFiltered = details.filter(detail => detail.href.includes('http'));
+
         linksFiltered.forEach(link => {
             let linkObject = {
                 'ruta': path.resolve(file),
-                'link': link
+                'link': link.href,
+                'text': link.text
             };
 
-            //console.log(options);
-           //if(options == ) 
-
-            response.push(linkObject);
-
+            if (options.find(option => option === '--validate')) {
+                fetchHref(link.href)
+                .then(status => {
+                    linkObject['status'] = status;
+                    console.log(linkObject);
+                });
+                
+            } 
+            
+                response.push(linkObject);
+            
+            
         }); 
-        
+
     });
+    
     return response;
 } 
 
+const fetchHref = (href) => {
+    return new Promise((resolve, reject) => {
+        fetch(href, { method: 'GET'})            
+        .then(res => { 
+            resolve(res.status);
+        })
+        .catch(error => console.log(error));
+    });
+}
 
 module.exports = {
     mdlinksReading
